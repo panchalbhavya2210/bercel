57bffa51b61aa040109d91e573727a6e4f92650c
Bhavya Panchal <panchalbhavya2210@gmail.com>
2024-07-27 20:57:34 +0530

    hello

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,22 @@
-"use client";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import ProductPage from "./components/product";
+import React from "react";
 
-export default function Home() {
-  const [product, setProduct] = useState(null);
-
-  useEffect(() => {
-    let apiUrl = "https://linode25.eqserver.net/mock_product_response.json";
-    axios.get(apiUrl).then((response) => setProduct(response.data));
-  }, []);
-
-  if (!product) {
-    return <div>loading...</div>;
-  } else {
-    return (
-      <main>
-        <h1>Main Page</h1>
-        <ProductPage config={product} />
-      </main>
-    );
+async function getRepo() {
+  const res = await fetch(
+    "https://linode25.eqserver.net/mock_product_response.json"
+  );
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
   }
+
+  return res.json();
+}
+
+export default async function Page() {
+  const repo = await getRepo();
+  console.log(repo.price);
+  return (
+    <div>
+      <p>Stars: {repo.price}</p>
+    </div>
+  );
 }
